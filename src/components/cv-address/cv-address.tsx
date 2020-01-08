fd685bd23e6f81046fa59992dee42eb4e48c2dd0
deciduously <ben@deciduously.com>
2020-01-08 13:23:19 -0500

    Update schema syntax

--- src/components/cv-address/cv-address.tsx
+++ src/components/cv-address/cv-address.tsx
@@ -12,13 +12,14 @@ export class CvAddress {
   @Prop() address: Address;
 
   render() {
-    return (
-      <p typeof="schema:PostalAddress" id="address">
-          <span property="schema:streetAddress">{this.address.street}</span><br />
-          <span property="schema:addressLocality">{this.address.locality.name}</span>, <abbr title={this.address.locality.state.fullName} property="schema:addressRegion">{this.address.locality.state.abbreviation}</abbr> <span property="schema:postalCode">{this.address.locality.postalCode}</span><br />
-          <span property="schema:addressCountry">{this.address.locality.state.country}</span>
-      </p>
-    );
+    if (this.address) {
+      return (
+        <p itemscope itemtype="https://schema.org/PostalAddress" id="address">
+          <span itemprop="streetAddress">{this.address.street}</span><br />
+          <span itemprop="addressLocality">{this.address.locality.name}</span>, <abbr title={this.address.locality.state.fullName} itemprop="addressRegion">{this.address.locality.state.abbreviation}</abbr> <span itemprop="postalCode">{this.address.locality.postalCode}</span><br />
+          <span itemprop="addressCountry">{this.address.locality.state.country}</span>
+        </p>
+      );
+    }
   }
-
 }
